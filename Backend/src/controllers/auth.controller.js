ed314a712801e021f6f7c84c08bffa6207b25527
Shikhar gupta <shikhargupta@Shikhars-MacBook-Air.local>
2026-07-02 23:01:01 +0530

    Handle Logout functionaklity from Backend & Frontend both

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,138 +1,145 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
-const { validateSignUpData, validateLoginData } = require("../utils/validation");
+const {
+  validateSignUpData,
+  validateLoginData,
+} = require("../utils/validation");
 
-const userModel = require('../models/user.model');
+const userModel = require("../models/user.model");
 
 const signup = async (req, res) => {
-    try {
-        const { email, firstName, lastName, password } = req.body;
-        const isValidationMessage = await validateSignUpData(req);
+  try {
+    const { email, firstName, lastName, password } = req.body;
+    const isValidationMessage = await validateSignUpData(req);
 
-        if (isValidationMessage)
-            return res.status(400).json({
-                status_code: 400,
-                error: {
-                    error_message: isValidationMessage
-                }
-            })
+    if (isValidationMessage)
+      return res.status(400).json({
+        status_code: 400,
+        error: {
+          error_message: isValidationMessage,
+        },
+      });
 
-        const isUserAlreadyExist = await userModel.findOne({
-            email
-        })
+    const isUserAlreadyExist = await userModel.findOne({
+      email,
+    });
 
-        if (isUserAlreadyExist) {
-            return res.status(409).json({
-                status_code: 409,
-                error: {
-                    error_message: "User has already exist."
-                }
-            })
-        }
-
-        const saltRounds = 10;
-        const passwordHash = await bcrypt.hash(password, saltRounds);
-
-        const newUser = await userModel.create({
-            firstName: firstName,
-            lastName: lastName,
-            email: email,
-            password: passwordHash
-        })
-
-        const token = await newUser.getJWT()
-
-        res.cookie("Token", token)
-
-        res.status(201).json({
-            status_code: 201,
-            success: true,
-            message: "User created successfully,",
-            data: {
-                firstName: newUser.firstName,
-                lastName: newUser.lastName,
-                email: newUser.email,
-            }
-        })
-    } catch (error) {
-        console.log("Auth controller error:", error);
-        return res.status(500).json({
-            return_message: "Internal Server Error",
-        });
+    if (isUserAlreadyExist) {
+      return res.status(409).json({
+        status_code: 409,
+        error: {
+          error_message: "User has already exist.",
+        },
+      });
     }
-}
+
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(password, saltRounds);
+
+    const newUser = await userModel.create({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: passwordHash,
+    });
+
+    const token = await newUser.getJWT();
+
+    res.cookie("Token", token);
+
+    res.status(201).json({
+      status_code: 201,
+      success: true,
+      message: "User created successfully,",
+      data: {
+        firstName: newUser.firstName,
+        lastName: newUser.lastName,
+        email: newUser.email,
+      },
+    });
+  } catch (error) {
+    console.log("Auth controller error:", error);
+    return res.status(500).json({
+      return_message: "Internal Server Error",
+    });
+  }
+};
 
 const login = async (req, res) => {
-    try {
-        const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-        const isValidationMessage = await validateLoginData(req);
-        if (isValidationMessage) {
-            return res.status(400).json({
-                status_code: 400,
-                error: {
-                    error_message: isValidationMessage
-                }
-            })
-        }
-
-        const userData = await userModel.findOne({ email });
-        if (!userData) {
-            return res.status(401).json({
-                status: false,
-                status_code: 401,
-                return_message: "User is not valid.",
-            });
-
-        }
-
-        const isPasswordMatch = await bcrypt.compare(password, userData.password);
-        if (!isPasswordMatch) {
-            return res.status(401).json({
-                status: false,
-                status_code: 401,
-                return_message: "Invalid Credentials",
-            });
-
-        }
-
-        const token = await userData.getJWT();
-        res.cookie("token", token, {
-            expires: new Date(Date.now() + 8 * 3600000)
-        })
-
-        res.status(200).json({
-            status: true,
-            return_message: "User logged in successfully.",
-            user: {
-                firstName: userData.firstName,
-                lastName: userData.lastName,
-                email: userData.email,
-                isPremium: userData.isPremium
-            }
-        })
-
-    } catch (error) {
-        console.log("Login Controller Error:", error);
-        return res.status(500).json({
-            status: false,
-            status_code: 500,
-            return_message: "Internal Server Error",
-        });
+    const isValidationMessage = await validateLoginData(req);
+    if (isValidationMessage) {
+      return res.status(400).json({
+        status_code: 400,
+        error: {
+          error_message: isValidationMessage,
+        },
+      });
     }
-}
+
+    const userData = await userModel.findOne({ email });
+    if (!userData) {
+      return res.status(401).json({
+        status: false,
+        status_code: 401,
+        return_message: "User is not valid.",
+      });
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, userData.password);
+    if (!isPasswordMatch) {
+      return res.status(401).json({
+        status: false,
+        status_code: 401,
+        return_message: "Invalid Credentials",
+      });
+    }
+
+    const token = await userData.getJWT();
+    res.cookie("token", token, {
+      expires: new Date(Date.now() + 8 * 3600000),
+    });
+
+    res.status(200).json({
+      status: true,
+      return_message: "User logged in successfully.",
+      user: {
+        firstName: userData.firstName,
+        lastName: userData.lastName,
+        email: userData.email,
+        isPremium: userData.isPremium,
+      },
+    });
+  } catch (error) {
+    console.log("Login Controller Error:", error);
+    return res.status(500).json({
+      status: false,
+      status_code: 500,
+      return_message: "Internal Server Error",
+    });
+  }
+};
 
 const logout = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        console.log("Logout Controller Error:", error);
-        return res.status(500).json({
-            status: false,
-            status_code: 500,
-            return_message: "Internal Server Error"
-        })
-    }
-}
+  try {
+    res.cookie("token", null, {
+      expires: new Date(Date.now()),
+    });
 
-module.exports = { signup, login, logout }
+    res.status(200).json({
+      status_code: 200,
+      return_message: "User logged out successfully.",
+    });
+  } catch (error) {
+    console.log("Logout Controller Error:", error);
+    return res.status(500).json({
+      status: false,
+      status_code: 500,
+      return_message: "Internal Server Error",
+    });
+  }
+};
+
+module.exports = { signup, login, logout };
